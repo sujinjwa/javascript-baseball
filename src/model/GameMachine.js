05009ee68c844b443f2a4c730bbb9a2d99a80c61
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-12-16 22:12:42 +0900

    refactor: forEach를 이용하여 메서드 내 길이 줄이기

--- src/model/GameMachine.js
+++ src/model/GameMachine.js
@@ -1,6 +1,7 @@
 const AnswerMaker = require('../AnswerMaker');
 const Validation = require('../utils/Validation');
 const { ANSWER_LENGTH } = require('../utils/constants');
+const Player = require('./Player');
 
 class GameMachine {
   #answer = [];
@@ -13,15 +14,9 @@ class GameMachine {
   compareAnswerWithNumbers(player) {
     const PLAYER_NUMBERS = player.getInputtedNumbers();
     this.#answer.forEach((number, index) => {
-      if (PLAYER_NUMBERS.includes(number)) {
-        if (PLAYER_NUMBERS[index] === number) {
-          this.#hint.strike += 1;
-          return;
-        }
-        this.#hint.ball += 1;
-        return;
-      }
-      this.#hint.nothing += 1;
+      if (!PLAYER_NUMBERS.includes(number)) return (this.#hint.nothing += 1);
+      if (PLAYER_NUMBERS[index] === number) return (this.#hint.strike += 1);
+      this.#hint.ball += 1;
     });
   }
 
